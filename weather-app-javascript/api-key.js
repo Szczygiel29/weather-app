ab5f382d6faace8a8531571ee1bdec7157c9862a
Jakub Szczygieł <116550165+Szczygiel29@users.noreply.github.com>
2023-04-02 19:44:48 +0200

    Delete api-key.js

--- weather-app-javascript/api-key.js
+++
@@ -1,4 +0,0 @@
-const APIkeyWeather = "6b99b7df96fb4419a11113403233001";
-const APIkeyPexels = "T5Z1cis9l2lnAfIfllxWqYPwsk26ZNSN98FtyIaGY2RfvL84PNl7e0ou";
-
-export default { APIkeyWeather, APIkeyPexels };
